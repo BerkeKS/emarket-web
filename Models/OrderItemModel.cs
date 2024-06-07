d2846e71a6049528407f434980ac1539b622108a
berkeks <berkekasabah@gmail.com>
2024-06-07 11:55:33 +0300

    Divide order database into two tables

---
+++ Models/OrderItemModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace emarket.Models;
+
+public class OrderItemModel {
+
+    [Key]
+    public String Id {get; set;}
+    [ForeignKey("Purchased")]
+    public String PurchasedItem {get; set;}
+    public ItemViewModel Purchased {get; set;}
+    
+    public int PurchasedAmount {get; set;}
+}
