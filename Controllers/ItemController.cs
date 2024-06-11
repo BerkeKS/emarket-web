73e4ebba28b250312e036c158af151781366a602
berkeks <berkekasabah@gmail.com>
2024-06-11 22:14:35 +0300

    Add unfinished version of buy action

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,5 +1,6 @@
 using emarket.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace emarket.Controllers;
 
@@ -14,5 +15,29 @@ public class ItemController : Controller
     public IActionResult Index(){
         IEnumerable<ItemViewModel> items = _db.Items;
         return View(items);
+    }
+
+    [HttpPost]
+    public void Buy(ItemViewModel item){
+        if (Request.Cookies["Login"] != null){
+            string activeUsername = Request.Cookies["Login"]!;
+            var order = _db.OrderUsers.Last(o => o.PurchaserUsername == activeUsername);
+            if (order.IsConfirmed){
+                //_db.OrderUsers.Add();
+                //_db.OrderItems.Add();
+            } else {
+                var ordered = _db.OrderItems.All(o => o.OrderId == order.OrderId && o.PurchasedItem == item.Name);
+                if(ordered){
+                    //Update the amount
+                    //Update total price
+                }
+                else{
+                    //Add a value of the new item
+                    //Update total price
+                }
+            }
+        } else{
+            //Return error message regarding login status
+        }
     }
 }
