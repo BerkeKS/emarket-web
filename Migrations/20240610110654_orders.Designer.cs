f8b96138181c90af92f45710fee20f029c47f4f5
berkeks <berkekasabah@gmail.com>
2024-06-11 22:17:25 +0300

    Add latest migrations

--- Migrations/20240610110654_orders.Designer.cs
+++ Migrations/20240610110654_orders.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace emarket.Migrations
 {
     [DbContext(typeof(EMarketDbContext))]
-    partial class EMarketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240610110654_orders")]
+    partial class orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240610110654_orders.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace emarket.Migrations
+{
+    /// <inheritdoc />
+    public partial class orders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    OrderId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PurchasedItem = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PurchasedAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Items_PurchasedItem",
+                        column: x => x.PurchasedItem,
+                        principalTable: "Items",
+                        principalColumn: "Name",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderUsers",
+                columns: table => new
+                {
+                    OrderId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PurchaserUsername = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    totalPrice = table.Column<double>(type: "float", nullable: false),
+                    IsConfirmed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderUsers", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_OrderUsers_Users_PurchaserUsername",
+                        column: x => x.PurchaserUsername,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_PurchasedItem",
+                table: "OrderItems",
+                column: "PurchasedItem");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderUsers_PurchaserUsername",
+                table: "OrderUsers",
+                column: "PurchaserUsername");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "OrderUsers");
+        }
+    }
+}
